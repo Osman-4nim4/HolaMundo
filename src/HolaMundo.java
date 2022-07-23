0268e2e3ec73b34d2a96504db48f90fa60068acf
Osman-4nim4 <60580797+Osman-4nim4@users.noreply.github.com>
2022-07-22 20:46:56 -0600

    Primera Version

---
+++ src/HolaMundo.java
@@ -0,0 +1,28 @@
+public class HolaMundo {
+    public static void  main (String[]args){
+
+        String saludar="Hola mundo desde java";
+        //System.out.println(saludar);
+        //System.out.println("saludar = " + saludar.toUpperCase());
+
+        var nombre="Osman Cordero";
+        int numero=10;
+        //System.out.println("numero = " + numero);
+        
+        var numero3="19";
+
+        //System.out.println("Mi nombre es "+nombre+" y mi edad es " +numero3);
+
+
+
+        String nombre3;
+
+        nombre3="Osman";
+
+        var _nombre="carlos";
+
+        System.out.println("Mi nombre es "+_nombre);
+    }
+
+}
+
